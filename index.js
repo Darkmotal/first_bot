59d33af9e2a798167b809ac89b6c59d425fffa08
Darkmotal <darkmotal@gmail.com>
2021-10-06 22:43:47 +0200

    New with webhook, hope it will work

--- index.js
+++ index.js
@@ -17,13 +17,7 @@ if (process.env.NODE_ENV === 'production') {
     bot = new TelegramBot(token, { polling: true });
 }
 
-const app = express();
-app.use(bodyParser.json());
-app.listen(process.env.PORT);
-app.post('/' + bot.token, (req, res) => {
-    bot.processUpdate(req.body);
-    res.sendStatus(200);
-});
+
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -45,4 +39,12 @@ bot.on('message', (msg) => {
 
     // send a message to the chat acknowledging receipt of their message
     bot.sendMessage(chatId, 'Received your message');
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.listen(process.env.PORT);
+app.post('/' + bot.token, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
 });
