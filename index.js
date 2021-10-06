082f70316d5936cd466eb6a897ca3d64f57e3dfa
Darkmotal <darkmotal@gmail.com>
2021-10-06 22:02:30 +0200

    New with webhook

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ require('dotenv').config()
 const token = process.env.T_TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+if (process.env.NODE_ENV === 'production') {
+    bot = new TelegramBot(token);
+    bot.setWebHook(process.env.HEROKU_URL + bot.token);
+} else {
+    bot = new TelegramBot(token, { polling: true });
+}
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
